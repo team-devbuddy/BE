6dcebf87a440bc1bd5202af5421d606cdaa6d5f6
hyeongjun im <limbs713@naver.com>
2024-06-28 16:47:19 +0900

    :sparkles: Member Domain template 추가

---
+++ src/main/java/com/ceos/beatbuddy/domain/user/entity/Member.java
@@ -0,0 +1,21 @@
+package com.ceos.beatbuddy.domain.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String loginId;
+    private Preferences preferences;
+}
