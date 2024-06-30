80a04df56dc7f35db657e895ac9926e54fb60deb
chlolive-new <otatop5819@gmail.com>
2024-06-30 22:00:24 +0900

    :sparkles: #2 Venue 패키지 생성

---
+++ src/main/java/com/ceos/beatbuddy/domain/venue/entity/Venue.java
@@ -0,0 +1,38 @@
+package com.ceos.beatbuddy.domain.venue.entity;
+
+import com.ceos.beatbuddy.domain.vector.entity.Vector;
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+public class Venue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String englishName;
+    private String koreanName;
+
+
+    @Transient
+    private Vector featureVector;
+
+    @Lob
+    private String featureVectorString;
+
+    public void setFeatureVector(Vector vector) {
+        this.featureVector = vector;
+        this.featureVectorString = vector.getElements().toString();
+    }
+
+    public Vector getFeatureVector() {
+        if (featureVector == null && featureVectorString != null) {
+            List<Double> elements = List.of(featureVectorString.replace("[", "").replace("]", "").split(","))
+                    .stream().map(String::trim).map(Double::parseDouble).collect(Collectors.toList());
+            featureVector = new Vector(elements);
+        }
+        return featureVector;
+    }
+}
---
+++ src/main/java/com/ceos/beatbuddy/domain/venue/repository/VenueRepository.java
@@ -0,0 +1,10 @@
+package com.ceos.beatbuddy.domain.venue.repository;
+
+
+import com.ceos.beatbuddy.domain.venue.entity.Venue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VenueRepository extends JpaRepository<Venue, Long> {
+    Long findByKoreanName(String koreanName);
+}
+
