51dfb3a6cadc3d99e95c1337019e8e6f170b65e9
chlolive-new <otatop5819@gmail.com>
2024-07-01 00:13:35 +0900

    :sparkles: #2 MemberGenre 추가

---
+++ src/main/java/com/ceos/beatbuddy/domain/member/repository/MemberGenreRepository.java
@@ -0,0 +1,8 @@
+package com.ceos.beatbuddy.domain.member.repository;
+
+import com.ceos.beatbuddy.domain.member.entity.MemberGenre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberGenreRepository extends JpaRepository<MemberGenre, Long> {
+
+}
