d8726bf6b8eca915f56b767c86e398da340f4f8e
chlolive-new <otatop5819@gmail.com>
2024-06-30 22:03:09 +0900

    :label: #2 Member 추천시스템을 위한 변수 추가

--- src/main/java/com/ceos/beatbuddy/domain/member/entity/Member.java
+++ src/main/java/com/ceos/beatbuddy/domain/member/entity/Member.java
@@ -1,13 +1,14 @@
 package com.ceos.beatbuddy.domain.member.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.ceos.beatbuddy.domain.vector.entity.Vector;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Builder
@@ -17,5 +18,27 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String loginId;
+
+    private String nickname;
+
+    @Transient
+    private Vector preferenceVector;
+
+    @Lob
+    private String preferenceVectorString;
+
+    public void setPreferenceVector(Vector vector) {
+        this.preferenceVector = vector;
+        this.preferenceVectorString = vector.getElements().toString();
+    }
+
+    public Vector getPreferenceVector() {
+        if (preferenceVector == null && preferenceVectorString != null) {
+            List<Double> elements = List.of(preferenceVectorString.replace("[", "").replace("]", "").split(","))
+                    .stream().map(String::trim).map(Double::parseDouble).collect(Collectors.toList());
+            preferenceVector = new Vector(elements);
+        }
+        return preferenceVector;
+    }
 
 }
