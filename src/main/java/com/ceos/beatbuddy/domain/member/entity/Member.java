0ec5eb1fe153d131cd62968af888398527218330
chlolive-new <otatop5819@gmail.com>
2024-07-01 00:14:41 +0900

    :recycle: #2 Member에서 Vector 관련 로직 삭제

--- src/main/java/com/ceos/beatbuddy/domain/member/entity/Member.java
+++ src/main/java/com/ceos/beatbuddy/domain/member/entity/Member.java
@@ -2,43 +2,22 @@ package com.ceos.beatbuddy.domain.member.entity;
 
 import com.ceos.beatbuddy.domain.vector.entity.Vector;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Entity
 @Builder
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long memberId;
     private String loginId;
 
     private String nickname;
 
-    @Transient
-    private Vector preferenceVector;
-
-    @Lob
-    private String preferenceVectorString;
-
-    public void setPreferenceVector(Vector vector) {
-        this.preferenceVector = vector;
-        this.preferenceVectorString = vector.getElements().toString();
-    }
-
-    public Vector getPreferenceVector() {
-        if (preferenceVector == null && preferenceVectorString != null) {
-            List<Double> elements = List.of(preferenceVectorString.replace("[", "").replace("]", "").split(","))
-                    .stream().map(String::trim).map(Double::parseDouble).collect(Collectors.toList());
-            preferenceVector = new Vector(elements);
-        }
-        return preferenceVector;
-    }
 
 }
