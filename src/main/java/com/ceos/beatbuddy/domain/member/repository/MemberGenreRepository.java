89b5caae668bd0d119032677caeeaf1761dc38c4
chlolive-new <otatop5819@gmail.com>
2024-07-01 21:09:18 +0900

    :recycle: #2 SQL 쿼리문 추가 및 수정

--- src/main/java/com/ceos/beatbuddy/domain/member/repository/MemberGenreRepository.java
+++ src/main/java/com/ceos/beatbuddy/domain/member/repository/MemberGenreRepository.java
@@ -1,8 +1,14 @@
 package com.ceos.beatbuddy.domain.member.repository;
 
+import com.ceos.beatbuddy.domain.member.entity.Member;
 import com.ceos.beatbuddy.domain.member.entity.MemberGenre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MemberGenreRepository extends JpaRepository<MemberGenre, Long> {
-
+    @Query("SELECT mg FROM MemberGenre mg WHERE mg.member = :member ORDER BY mg.createdAt DESC LIMIT 1")
+    Optional<MemberGenre> findLatestByMember(@Param("member") Member member);
 }
