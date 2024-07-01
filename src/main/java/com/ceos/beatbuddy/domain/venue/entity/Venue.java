78bce865a13161f335c62e9a9a1dad07914af2c3
chlolive-new <otatop5819@gmail.com>
2024-07-01 21:07:45 +0900

    :recycle: BaseTimeEntity 적용

--- src/main/java/com/ceos/beatbuddy/domain/venue/entity/Venue.java
+++ src/main/java/com/ceos/beatbuddy/domain/venue/entity/Venue.java
@@ -3,36 +3,23 @@ package com.ceos.beatbuddy.domain.venue.entity;
 import com.ceos.beatbuddy.domain.vector.entity.Vector;
 import com.ceos.beatbuddy.global.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Venue extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long venueId;
 
     private String englishName;
     private String koreanName;
 
-    @Transient
-    private Vector featureVector;
 
-    @Lob
-    private String featureVectorString;
-
-    public void setFeatureVector(Vector vector) {
-        this.featureVector = vector;
-        this.featureVectorString = vector.getElements().toString();
-    }
-
-    public Vector getFeatureVector() {
-        if (featureVector == null && featureVectorString != null) {
-            List<Double> elements = List.of(featureVectorString.replace("[", "").replace("]", "").split(","))
-                    .stream().map(String::trim).map(Double::parseDouble).collect(Collectors.toList());
-            featureVector = new Vector(elements);
-        }
-        return featureVector;
-    }
 }
