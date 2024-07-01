bf3138f187741926beb46fd9ce633bb8c83008a6
chlolive-new <otatop5819@gmail.com>
2024-07-01 21:09:18 +0900

    :recycle: SQL 쿼리문 추가 및 수정

--- src/main/java/com/ceos/beatbuddy/domain/member/repository/MemberRepository.java
+++ src/main/java/com/ceos/beatbuddy/domain/member/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package com.ceos.beatbuddy.domain.member.repository;
 import com.ceos.beatbuddy.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Long findByLoginId(String loginId);
+    Optional<Member> findByMemberId(Long memberId);
 }
--- src/main/java/com/ceos/beatbuddy/domain/venue/repository/VenueRepository.java
+++ src/main/java/com/ceos/beatbuddy/domain/venue/repository/VenueRepository.java
@@ -4,9 +4,12 @@ package com.ceos.beatbuddy.domain.venue.repository;
 import com.ceos.beatbuddy.domain.venue.entity.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface VenueRepository extends JpaRepository<Venue, Long> {
     Long findByKoreanName(String koreanName);
 
+    Optional<Venue> findByVenueId(Long venueId);
 }
 
