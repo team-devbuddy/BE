6978461813d1b7926b44cc66ce3abcdf4f2dc1c6
chlolive-new <otatop5819@gmail.com>
2024-07-02 15:21:47 +0900

    :recycle: #2 error code 추가

--- src/main/java/com/ceos/beatbuddy/domain/member/exception/MemberGenreErrorCode.java
+++ src/main/java/com/ceos/beatbuddy/domain/member/exception/MemberGenreErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum MemberGenreErrorCode {
 
+    MEMBER_GENRE_OVER_REQUEST(HttpStatus.BAD_REQUEST, "존재하는 선호 장르 벡터보다 요청 수가 많습니다"),
     MEMBER_GENRE_NOT_EXIST(HttpStatus.NOT_FOUND, "선호 장르가 존재하지 않습니다");
 
     private final HttpStatus httpStatus;
