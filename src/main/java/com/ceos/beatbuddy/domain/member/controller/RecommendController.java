a6e94a1c4fe346c6db4d42100f4f4717eb999fdc
hyeongjun im <limbs713@naver.com>
2024-07-05 19:09:14 +0900

    :memo: User 도메인의 Controller DOC 추가

--- src/main/java/com/ceos/beatbuddy/domain/member/controller/RecommendController.java
+++ src/main/java/com/ceos/beatbuddy/domain/member/controller/RecommendController.java
@@ -2,7 +2,15 @@ package com.ceos.beatbuddy.domain.member.controller;
 
 
 import com.ceos.beatbuddy.domain.member.application.RecommendService;
+import com.ceos.beatbuddy.domain.member.dto.MemberVectorResponseDTO;
 import com.ceos.beatbuddy.domain.venue.dto.VenueResponseDTO;
+import com.ceos.beatbuddy.global.ResponseTemplate;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,16 +20,45 @@ import java.util.List;
 @RestController
 @RequestMapping("/recommend")
 @RequiredArgsConstructor
+@Tag(name = "Recommend Controller", description = "사용자의 장르와 분위기에 따른 베뉴를 추천하는 컨트롤러")
 public class RecommendController {
 
     private final RecommendService recommendService;
 
     @GetMapping("/genre/{memberId}")
+    @Operation(summary = "장르별 베뉴 추천\n",
+            description = "사용자의 장르 선호도에 의해 추출된 추천 베뉴를 조회합니다.\n"
+                    + "현재는 2개의 베뉴를 추천하도록 설정했습니다.(추후 변경 가능)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "장르별 추천 베뉴를 조회하는데 성공했습니다."
+                    , content = @Content(mediaType = "application/json"
+                    , schema = @Schema(implementation = VenueResponseDTO.class))),
+            @ApiResponse(responseCode = "404", description = "요청한 유저가 존재하지 않습니다",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseTemplate.class))),
+            @ApiResponse(responseCode = "404", description = "유저의 장르 선호도가 존재하지 않습니다",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseTemplate.class)))
+    })
     public ResponseEntity<List<VenueResponseDTO>> recommendByGenre(@PathVariable final Long memberId) {
         return ResponseEntity.ok(recommendService.recommendVenuesByGenre(memberId, 2L));
     }
 
     @GetMapping("/mood/{memberId}")
+    @Operation(summary = "분위기별 베뉴 추천",
+            description = "사용자의 분위기 선호도에 의해 추출된 추천 베뉴를 조회합니다.\n"
+                    + "현재는 2개의 베뉴를 추천하도록 설정했습니다.(추후 변경 가능)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "분위기별 추천 베뉴를 조회하는데 성공했습니다."
+                    , content = @Content(mediaType = "application/json"
+                    , schema = @Schema(implementation = VenueResponseDTO.class))),
+            @ApiResponse(responseCode = "404", description = "요청한 유저가 존재하지 않습니다",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseTemplate.class))),
+            @ApiResponse(responseCode = "404", description = "유저의 분위기 선호도가 존재하지 않습니다",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseTemplate.class)))
+    })
     public ResponseEntity<List<VenueResponseDTO>> recommendByMood(@PathVariable final Long memberId) {
         return ResponseEntity.ok(recommendService.recommendVenuesByMood(memberId, 2L));
     }
--- src/main/java/com/ceos/beatbuddy/domain/venue/controller/VenueGenreController.java
+++ src/main/java/com/ceos/beatbuddy/domain/venue/controller/VenueGenreController.java
@@ -2,6 +2,8 @@ package com.ceos.beatbuddy.domain.venue.controller;
 
 import com.ceos.beatbuddy.domain.venue.application.VenueGenreService;
 import com.ceos.beatbuddy.domain.venue.dto.VenueVectorResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,16 +13,28 @@ import java.util.Map;
 @RestController
 @RequestMapping("/venue-genre")
 @RequiredArgsConstructor
+@Tag(name = "VenueGenre Controller", description = "베뉴에 대한 장르를 추가, 수정하는 컨트롤러\n"
+        + "현재는 베뉴 업자에 대한 페이지가 없으므로 아직 구현할 것이 없습니다\n"
+        + "(잘못 알고 있다면 알려주세요)")
 public class VenueGenreController {
     private final VenueGenreService venueGenreService;
 
     @PostMapping("/{venueId}")
-    public ResponseEntity<VenueVectorResponseDTO> addGenrePreference(@PathVariable Long venueId, @RequestBody Map<String, Double> preferences) {
+    @Operation(summary = "베뉴 장르 선호도 추가",
+            description = "베뉴의 장르 선호도를 추가합니다")
+
+    public ResponseEntity<VenueVectorResponseDTO> addGenrePreference(@PathVariable Long venueId,
+                                                                     @RequestBody Map<String, Double> preferences) {
         return ResponseEntity.ok(venueGenreService.addGenreVector(venueId, preferences));
     }
 
     @PatchMapping("/{venueId}")
-    public ResponseEntity<VenueVectorResponseDTO> updateGenrePreference(@PathVariable Long venueId, @RequestBody Map<String, Double> preferences) {
+    @Operation(summary = "베뉴 장르 선호도 업데이트",
+            description = "베뉴 장르 선호도를 업데이트 합니다.\n"
+                    + "일반적으로 베뉴 장르는 크게 변하지 않으며 장르가 없을 수 없으므로\n"
+                    + "DeleteMapping은 구현하지 않았습니다.")
+    public ResponseEntity<VenueVectorResponseDTO> updateGenrePreference(@PathVariable Long venueId,
+                                                                        @RequestBody Map<String, Double> preferences) {
         return ResponseEntity.ok(venueGenreService.updateGenreVector(venueId, preferences));
     }
 }
