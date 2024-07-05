fe1c8149c13eaea2e54299a2ecdfc01f12731a6a
hyeongjun im <limbs713@naver.com>
2024-07-05 19:25:14 +0900

    :wrench: redirectUrl 수정
    실행하는 애플리케이션 도메인에 따라
    자동으로 baseUri를 채우도록 변경

--- src/main/java/com/ceos/beatbuddy/global/config/oauth/CustomClientRegistrationRepo.java
+++ src/main/java/com/ceos/beatbuddy/global/config/oauth/CustomClientRegistrationRepo.java
@@ -23,12 +23,13 @@ public class CustomClientRegistrationRepo {
     }
 
     private ClientRegistration kakaoClientRegistration() {
+        String redirectUriTemplate = "{baseUrl}/login/oauth2/code/{registrationId}";
         return ClientRegistration.withRegistrationId("kakao")
                 .clientId(clientId)
                 .clientSecret(clientSecret)
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("http://localhost:8080/login/oauth2/code/kakao")
+                .redirectUri(redirectUriTemplate)
                 .authorizationUri("https://kauth.kakao.com/oauth/authorize")
                 .tokenUri("https://kauth.kakao.com/oauth/token")
                 .userInfoUri("https://kapi.kakao.com/v2/user/me")
