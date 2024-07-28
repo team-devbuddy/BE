31795a71129e0dd9871b601d7bfee12546c84b42
hyeongjun im <limbs713@naver.com>
2024-07-28 18:50:53 +0900

    :recycle: refresh token의 id값 수정

--- src/main/java/com/ceos/beatbuddy/global/config/jwt/redis/RefreshToken.java
+++ src/main/java/com/ceos/beatbuddy/global/config/jwt/redis/RefreshToken.java
@@ -9,9 +9,7 @@ import org.springframework.data.redis.core.index.Indexed;
 @RedisHash(value = "refreshToken", timeToLive = 60 * 60 * 24 * 14)
 public class RefreshToken {
     @Id
-    private String id;
     private String refreshToken;
-    @Indexed
     private String accessToken;
 
     public RefreshToken(String refreshToken, String accessToken) {
