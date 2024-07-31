0051abd7fa4b3a40e0deb831cb4d6ea9d531b5c8
hyeongjun im <limbs713@naver.com>
2024-08-01 01:07:26 +0900

    :sparkles: refresh 토큰 error code 추가

--- src/main/java/com/ceos/beatbuddy/global/config/oauth/exception/OauthErrorCode.java
+++ src/main/java/com/ceos/beatbuddy/global/config/oauth/exception/OauthErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 public enum OauthErrorCode {
 
     LOGOUT_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "로그아웃에 실패했습니다"),
-    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 Token입니다.");
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 Token입니다."),
+    EXPIRED_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "만료된 토큰입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST,"잘못된 토큰 타입입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
