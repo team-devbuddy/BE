bfd1860de9dc5689289ca05284e2189cf77f93b1
hyeongjun im <limbs713@naver.com>
2025-01-17 12:17:17 +0900

    :bug: PostRequestDto에 Type에 따라 구현체 정의하도록 수정

--- src/main/java/com/ceos/beatbuddy/domain/post/dto/PostRequestDto.java
+++ src/main/java/com/ceos/beatbuddy/domain/post/dto/PostRequestDto.java
@@ -1,15 +1,30 @@
 package com.ceos.beatbuddy.domain.post.dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
 import org.springframework.web.multipart.MultipartFile;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXTERNAL_PROPERTY,  // URL의 type을 사용할 것이므로 JSON 내부에는 type 정보 불필요
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = PostRequestDto.FreePostRequestDto.class, name = "free"),
+        @JsonSubTypes.Type(value = PostRequestDto.PiecePostRequestDto.class, name = "piece")
+})
 public sealed interface PostRequestDto {
     String title();
+
     String content();
+
     List<MultipartFile> images();
+
     Long venueId();
+
     @Builder
     record PiecePostRequestDto(
             String title,
@@ -19,7 +34,8 @@ public sealed interface PostRequestDto {
             int totalPrice,
             int totalMembers,
             LocalDateTime eventDate
-    ) implements PostRequestDto {}
+    ) implements PostRequestDto {
+    }
 
     @Builder
     record FreePostRequestDto(
@@ -27,5 +43,6 @@ public sealed interface PostRequestDto {
             String content,
             List<MultipartFile> images,
             Long venueId
-    ) implements PostRequestDto {}
+    ) implements PostRequestDto {
+    }
 }
